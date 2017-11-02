c2ba32f2de2a0fa7cd5b66fa42dbb7945d02fb53
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2017-11-02 14:20:37 +0000

    test3

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<?php
-   if( $_POST["name"] || $_POST["age"] ) {
-      if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
-         die ("invalid name and name should be alpha");
-      }
-      $name = $_POST['name'];
-      $age = $_POST['age'];
-       
-      echo($name);
-      exit();
-   }
-?>
+
 <html>
    <body>
-        <? echo($name); ?>
+        <?php
+           if( $_POST["name"] || $_POST["age"] ) {
+              if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
+                 die ("invalid name and name should be alpha");
+              }
+              $name = $_POST['name'];
+              $age = $_POST['age'];
+
+              $file = fopen("data.txt", "w") or die("Unable to open file!");
+              fwrite($file, $age);
+              fclose($file);
+               
+               $myfile = fopen("data.txt", "r") or die("Unable to open file!");
+               echo fgets($myfile);
+               fclose($myfile);
+              
+              exit();
+           }
+        ?>
    </body>
 </html>
