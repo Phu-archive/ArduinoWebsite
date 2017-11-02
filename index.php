f76567348fb02fe3b2e5b3480a592b1bb24a7d60
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2017-11-02 13:34:03 +0000

    Test 2

--- index.php
+++ index.php
@@ -9,14 +9,3 @@
       exit();
    }
 ?>
-<html>
-   <body>
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "POST">
-         Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
-         <input type = "submit" />
-      </form>
-   
-   </body>
-</html>
