40086285915a63b0ec1c4a87c6b2ac5d4d5ea9fe
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2017-11-02 20:54:43 +0000

    Add clean

--- add.php
+++ add.php
@@ -1,13 +1,15 @@
 <?php
+           if( $_POST["clean"]){
+               file_put_contents("data.txt", "");
+               echo "works";
+           }
            if( $_POST["x"] || $_POST["y"] ) {
 
               $x = $_POST['x'];
               $y = $_POST['y'];
                
-              $add = $x + " " + $y + "\n";
+              $add = $x + " " + $y + '\n';
                
-              
-
               $file = file_put_contents('data.txt', $add.PHP_EOL , FILE_APPEND | LOCK_EX);
             
               fclose($file);
