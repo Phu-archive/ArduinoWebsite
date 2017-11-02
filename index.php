c963adbee6df65b5c747bc327be333a20b02a945
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2017-11-02 13:57:35 +0000

    test7

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
+<?php
+   if( $_POST["name"] || $_POST["age"] ) {
+      if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
+         die ("invalid name and name should be alpha");
+      }
+      $name = $_POST['name'];
+      $age = $_POST['age'];
+      
+      exit();
+   }
+?>
 <html>
-<body>
-
-Welcome <?php printf($_POST["name"]); ?><br>
-Your email address is: <?php printf($_POST["age"]); ?>
-
-</body>
+   <body>
+   
+      <?php echo $name ?>
+   
+   </body>
 </html>
