3acaabbfc718c1cc094bc49a3abcd3b7c9c46195
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2017-11-02 13:37:27 +0000

    test4

--- index.php
+++ index.php
@@ -1,22 +1,2 @@
-<?php
-   if( $_POST["name"] || $_POST["age"] ) {
-      if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
-         die ("invalid name and name should be alpha");
-      }
-      echo "Welcome ". $_POST['name']. "<br />";
-      echo "You are ". $_POST['age']. " years old.";
-      
-      exit();
-   }
-?>
-<html>
-   <body>
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "POST">
-         Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
-         <input type = "submit" />
-      </form>
-   
-   </body>
-</html>
+Hi <?php echo htmlspecialchars($_POST['name']); ?>.
+You are <?php echo (int)$_POST['age']; ?> years old.
