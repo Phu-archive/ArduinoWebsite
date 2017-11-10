e9515ebf20edf885eb29a5a2a5c30fbe25d384eb
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2017-11-10 11:27:53 +0000

    bootstrap stuff

--- index.php
+++ index.php
@@ -7,38 +7,39 @@
        <script src="sketch.js"></script>
    </head>
 <body>
-   <h1 class"display-3">Log Information</h1>
+    <div class="container">
+        <h1 class"display-3">Log Information</h1>
 
 
-      <?php
+          <?php
 
-        $fh = fopen('data.txt','r');
-        while ($line = fgets($fh)) {
-          echo($line);
-        }
-        
-        fclose($fh);
-      ?>
-    <button type="button" class="btn btn-primary btn-lg">Delete</button>
-    
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $("button").click(function(){
+            $fh = fopen('data.txt','r');
+            while ($line = fgets($fh)) {
+              echo($line);
+            }
 
-                $.ajax({
-                    type: 'POST',
-                    url: 'delete.php',
-                    success: function(data) {
-                        alert("Delete Successful");
-                    }
-                });
-       });
-    });
-    </script>
-    
-    <div id="sketch-holder">
-      <!-- Our sketch will go here! -->
+            fclose($fh);
+          ?>
+        <button type="button" class="btn btn-primary btn-lg">Delete</button>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $("button").click(function(){
+
+                    $.ajax({
+                        type: 'POST',
+                        url: 'delete.php',
+                        success: function(data) {
+                            alert("Delete Successful");
+                        }
+                    });
+           });
+        });
+        </script>
+
+        <div id="sketch-holder">
+          <!-- Our sketch will go here! -->
+        </div>
     </div>
-
 </body>
 </html>
